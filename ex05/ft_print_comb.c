aabe099bbf5695fbb9fb70e90aa91f9073fd1f1b
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-05 12:12:40 +0200

    Edit ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,37 +6,47 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 09:55:20 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/05 10:27:44 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/05 12:10:01 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_print_comb(void)
 {
-	int i1 = '0';
-	int i2 = '1';
-	int i3 = '2';
+	int	i1;
+	int	i2;
+	int	i3;
 
-	while (i3 <= '9')
+	i1 = '0';
+	while (i1 <= '7')
 	{
-		write(1, &i1, 1);
-		write(1, &i2, 1);
-		write(1, &i3, 1);
-
-		i1++;
-		i2++;
-		i3++;
-
-		if (i3 != 9)
+		i2 = i1 + 1;
+		while (i2 <= '8')
 		{
-			write(1, " ", 1);
+			i3 = i2 + 1;
+			while (i3 <= '9')
+			{
+				ft_putchar(i1);
+				ft_putchar(i2);
+				ft_putchar(i3);
+				if (i1 != '7')
+					write(1, ", ", 2);
+				i3++;
+			}
+			i2++;
 		}
+		i1++;
 	}
 }
 
 int	main(void)
 {
 	ft_print_comb();
-	return 0;
+	return (0);
 }
